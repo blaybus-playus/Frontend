d3221723b01ce5e3228785358347bbf14e255105
JinwooKim <rlawlsdn9583@gmail.com>
2025-01-07 16:45:46 +0900

    design: 헤더 컴포넌트 구현

---
+++ components/common/Header.tsx
@@ -0,0 +1,22 @@
+import { bell } from '@/assets/images'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full h-[104px] bg-white z-1000 border-b">
+      <div className="absolute bottom-0 w-full h-[56px] flex justify-center items-center">
+        <Image
+          src={bell}
+          alt="Notifications"
+          width={24}
+          height={24}
+          className="absolute left-[1rem] cursor-pointer"
+        />
+        <span className="text-lg font-semibold">동적주소</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
