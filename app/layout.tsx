a051e396e0c377213419f713ec4cad4991461b88
Ray <rlawlsdn9583@gmail.com>
2025-01-09 23:24:24 +0900

    setting: next-pwa 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/common/Header";
 export const metadata: Metadata = {
   title: "DoStep",
   description: "다음 단계로 계속 나아가자",
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default function RootLayout({
@@ -14,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="pt-[104px] bg-primary-100 overflow-x-hidden">
+      <body className="bg-primary-100 pt-[104px]">
         <Header />
         <div className="w-full px-[16px]">
           {children}
