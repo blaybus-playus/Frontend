320ecd45337f1c515b14a55316aaefd77302e08e
Ray <rlawlsdn9583@gmail.com>
2025-01-09 23:54:37 +0900

    style: 아이콘 애셋 추가

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,1 +1,8 @@
 export { default as bell } from "./bell.svg";
+export { default as user } from "./icon Button=user.svg";
+export { default as quest } from "./icon Button=user.svg";
+export { default as noticeBoard } from "./icon Button=user.svg";
+export { default as home } from "./icon Button=user.svg";
+export { default as exp } from "./icon Button=user.svg";
+export { default as search } from "./icon Button=user.svg";
+export { default as back } from "./icon Button=user.svg";
