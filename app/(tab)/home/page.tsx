5f0bb87f079cc4b7eaf5e08bcfe7c5e4871112e6
Ray <rlawlsdn9583@gmail.com>
2025-01-12 02:41:07 +0900

    setting: 높이 테스트 태그 생성

--- app/(tab)/home/page.tsx
+++ app/(tab)/home/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-
+      <div className='h-[300px] bg-primary-100' />
+      <div className='h-[300px] bg-primary-200' />
+      <div className='h-[300px] bg-primary-300' />
+      <div className='h-[300px] bg-primary-400' />
+      <div className='h-[300px] bg-primary-500' />
     </div>
   )
 }
