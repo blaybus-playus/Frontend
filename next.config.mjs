dbda63feb66f8228398a9816e26cb312e56a8f68
Ray <rlawlsdn9583@gmail.com>
2025-01-12 03:35:31 +0900

    fix: PWA 세팅 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
+const prod = process.env.NODE_ENV === 'production';
+
 const nextConfig = withPWA({
   dest: 'public', // PWA 관련 파일이 저장될 디렉토리
+  register: true,
+  skipWaiting: true,
+  disable: prod ? false : true
 });
 
 export default nextConfig;
