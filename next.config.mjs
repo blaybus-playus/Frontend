5714e655ce85db10c02c5c2c036f0d42a6420ddf
Ray <rlawlsdn9583@gmail.com>
2025-01-12 03:10:10 +0900

    setting: PWA 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
   dest: 'public', // PWA 관련 파일이 저장될 디렉토리
+  register: true, // PWA 등록
+  skipWaiting: true, // 대기 중인 SW 활성화
+  reactStrictMode: true,
 });
 
 export default nextConfig;
