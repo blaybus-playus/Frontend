8d54891c0b28bc41a9358150012986cc70011c40
Ray <rlawlsdn9583@gmail.com>
2025-01-11 14:33:51 +0900

    setting: PWA 서비스 워커 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public', // PWA 관련 파일이 저장될 디렉토리
+});
 
 export default nextConfig;
