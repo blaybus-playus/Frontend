f01601064b43875d0bdf09036e13e046d76d3075
Ray <rlawlsdn9583@gmail.com>
2025-01-13 00:24:48 +0900

    style: 로딩화면 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
 
   return (
-    <div>
-      isLoading...
+    <div className="flex justify-center items-center w-full h-[100vh] title-semibold-20">
+      정보 불러오는 중...
     </div>
   )
 }
