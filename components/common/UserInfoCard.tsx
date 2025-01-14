49e3d8566870c8ba307e932397c13bdc7dcc69d8
Ray <rlawlsdn9583@gmail.com>
2025-01-14 22:50:55 +0900

    feat: 유저 카드 API 연결

--- components/common/UserInfoCard.tsx
+++ components/common/UserInfoCard.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Image from 'next/image'
 import React from 'react'
-import { level_badge_01, man1_avatar } from '@/assets/images'
+import { level_badge_01, level_badge_02, level_badge_03, level_badge_04, level_badge_05, man1_avatar } from '@/assets/images'
 import { Progress } from '../ui/progress'
+import { useAtom } from 'jotai'
+import { userInfoAtom } from '@/store/atoms'
 
 const UserInfoCard = ({ bar }: { bar: boolean }) => {
+  const [userInfo, setUserInfo] = useAtom(userInfoAtom);
+  const level = userInfo.level.slice(0, 2);
+
   return (
     <div className="bg-gray-50 rounded-2xl w-full px-2.5 py-4">
       <div className="flex flex-col gap-6">
@@ -19,25 +26,31 @@ const UserInfoCard = ({ bar }: { bar: boolean }) => {
           <div className="space-y-2.5">
             <div className="flex gap-2">
               <span className="body-medium-14 text-gray-800">이름 :</span>
-              <span className="body-semibold-14 text-gray-950">홍길동</span>
+              <span className="body-semibold-14 text-gray-950">{userInfo.name}</span>
             </div>
             <div className="flex gap-2">
               <span className="body-medium-14 text-gray-800">사번 :</span>
-              <span className="body-semibold-14 text-gray-950">123456789</span>
+              <span className="body-semibold-14 text-gray-950">{userInfo.employeeId}</span>
             </div>
             <div className="flex gap-2">
               <span className="body-medium-14 text-gray-800">소속 :</span>
-              <span className="body-semibold-14 text-gray-950">음성 1센터</span>
+              <span className="body-semibold-14 text-gray-950">{userInfo.department}</span>
             </div>
             {bar && <>
               <div className="flex gap-2">
                 <span className="body-medium-14 text-gray-800">레벨 :</span>
-                <span className="body-semibold-14 text-gray-950">F1-1</span>
+                <span className="body-semibold-14 text-gray-950">{userInfo.level}</span>
               </div>
             </>}
           </div>
           <Image
-            src={level_badge_01}
+            src={
+              level === 'F1' ? level_badge_01 :
+                level === 'F2' ? level_badge_02 :
+                  level === 'F3' ? level_badge_03 :
+                    level === 'F4' ? level_badge_04 :
+                      level_badge_05
+            }
             alt='레벨 뱃지'
             width={60}
             height={60}
