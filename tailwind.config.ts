67e12c627c5c6bbf5a30923d1f705e98d0d72d82
JinwooKim <rlawlsdn9583@gmail.com>
2025-01-14 09:46:52 +0900

    setting: shadcn 아코디언 컴포넌트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,33 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        ...COLORS_SYSTEM
-      }
-    }
+  	extend: {
+  		colors: {
+                ...COLORS_SYSTEM
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [
     require("tailwindcss-animate"),
