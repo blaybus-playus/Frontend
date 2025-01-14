aba4defb1bf97f3a402f6c758ab4e189ed8fc7a8
JinwooKim <rlawlsdn9583@gmail.com>
2025-01-14 13:27:26 +0900

    feat: 대략적인 퍼블리싱 완료

--- app/(tab)/quest/leader/page.tsx
+++ app/(tab)/quest/leader/page.tsx
@@ -1,9 +1,16 @@
+import QuestHistory from '@/components/quest/QuestHistory'
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
+  const category = ["월특근", "업무개선", "업무1", "업무2"];
 
+  return (
+    <div className='space-y-3.5'>
+      {category.map((item, index) => (
+        <>
+          <QuestHistory title={item} index={index} />
+        </>
+      ))}
     </div>
   )
 }
