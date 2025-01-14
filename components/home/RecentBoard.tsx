5fc55a8ceb051b1d2fc6ae6281ed732a909d09a8
Ray <rlawlsdn9583@gmail.com>
2025-01-14 22:07:09 +0900

    design: 카드 배경색 변경

--- components/home/RecentBoard.tsx
+++ components/home/RecentBoard.tsx
@@ -20,7 +20,7 @@ const RecentBoard = () => {
           />
         ))}
       </div>
-      <div className='space-y-3.5 bg-primary-100 rounded-2xl px-4 py-5'>
+      <div className='space-y-3.5 bg-[#FFF3F0] rounded-2xl px-4 py-5'>
         <div className='flex items-center gap-1.5'>
           <h3 className='text-primary-400 body-semibold-12'>[전체 공지]</h3>
           <span className='body-regular-12 text-gray-500'>00분 전</span>
@@ -29,7 +29,7 @@ const RecentBoard = () => {
           AAA 프로젝트 신설 (경험치 500 do, 신청 마감 ~10/31), 잡초이스 공고(신청 마감 ~11/20)
         </div>
       </div>
-      <div className='space-y-3.5 bg-primary-100 rounded-2xl px-4 py-5'>
+      <div className='space-y-3.5 bg-[#FFF3F0] rounded-2xl px-4 py-5'>
         <div className='flex items-center gap-1.5'>
           <h3 className='text-primary-400 body-semibold-12'>[전체 공지]</h3>
           <span className='body-regular-12 text-gray-500'>00분 전</span>
@@ -38,7 +38,7 @@ const RecentBoard = () => {
           AAA 프로젝트 신설 (경험치 500 do, 신청 마감 ~10/31), 잡초이스 공고(신청 마감 ~11/20)
         </div>
       </div>
-      <div className='space-y-3.5 bg-primary-100 rounded-2xl px-4 py-5'>
+      <div className='space-y-3.5 bg-[#FFF3F0] rounded-2xl px-4 py-5'>
         <div className='flex items-center gap-1.5'>
           <h3 className='text-primary-400 body-semibold-12'>[전체 공지]</h3>
           <span className='body-regular-12 text-gray-500'>00분 전</span>
